3cc4c508a77d392ee3354dbd938720aed16adadd
Meier-Stefan <42180739+Meier-Stefan@users.noreply.github.com>
2024-03-21 15:50:11 +0000

    Refactor AppDbContext class and add XML documentation

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -5,16 +5,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesTestSample.Data
 {
+    /// <summary>
+    /// Represents the database context for the application.
+    /// </summary>
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options for configuring the context.</param>
         public AppDbContext(DbContextOptions<AppDbContext> options) 
             : base(options)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the collection of messages in the database.
+        /// </summary>
         public virtual DbSet<Message> Messages { get; set; }
 
         #region snippet1
+        /// <summary>
+        /// Retrieves all messages from the database asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the list of messages.</returns>
         public async virtual Task<List<Message>> GetMessagesAsync()
         {
             return await Messages
@@ -25,6 +39,11 @@ namespace RazorPagesTestSample.Data
         #endregion
 
         #region snippet2
+        /// <summary>
+        /// Adds a new message to the database asynchronously.
+        /// </summary>
+        /// <param name="message">The message to add.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public async virtual Task AddMessageAsync(Message message)
         {
             await Messages.AddAsync(message);
@@ -33,6 +52,10 @@ namespace RazorPagesTestSample.Data
         #endregion
 
         #region snippet3
+        /// <summary>
+        /// Deletes all messages from the database asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public async virtual Task DeleteAllMessagesAsync()
         {
             foreach (Message message in Messages)
@@ -45,6 +68,11 @@ namespace RazorPagesTestSample.Data
         #endregion
 
         #region snippet4
+        /// <summary>
+        /// Deletes a message from the database asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the message to delete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public async virtual Task DeleteMessageAsync(int id)
         {
             var message = await Messages.FindAsync(id);
@@ -57,12 +85,19 @@ namespace RazorPagesTestSample.Data
         }
         #endregion
 
+        /// <summary>
+        /// Initializes the database with a set of seeding messages.
+        /// </summary>
         public void Initialize()
         {
             Messages.AddRange(GetSeedingMessages());
             SaveChanges();
         }
 
+        /// <summary>
+        /// Gets a list of seeding messages.
+        /// </summary>
+        /// <returns>A list of seeding messages.</returns>
         public static List<Message> GetSeedingMessages()
         {
             return new List<Message>()
